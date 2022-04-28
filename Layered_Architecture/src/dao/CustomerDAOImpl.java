150bc78a2c914851eba54b0bec74bb5ceaa50df5
Gihan42 <gihanm729@gmail.com>
2022-04-28 21:15:45 +0530

    Aded Pos System

---
+++ Layered_Architecture/src/dao/CustomerDAOImpl.java
@@ -0,0 +1,33 @@
+package dao;
+
+import db.DBConnection;
+import model.CustomerDTO;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class CustomerDAOImpl {
+    public ArrayList<CustomerDTO> getAllCustomer() throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        Statement stm = connection.createStatement();
+        ResultSet rst = stm.executeQuery("SELECT * FROM Customer");
+        ArrayList <CustomerDTO> allCustomer=new ArrayList<>();
+        while(rst.next()){
+            String id = rst.getString(1);
+            String name = rst.getString(2);
+            String address = rst.getString(3);
+            allCustomer.add(new CustomerDTO(id,name,address));
+        }
+        return allCustomer;
+    }
+    public boolean saveCustomer(CustomerDTO dto) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("INSERT INTO Customer (id,name, address) VALUES (?,?,?)");
+        pstm.setString(1, dto.getId());
+        pstm.setString(2, dto.getName());
+        pstm.setString(3, dto.getAddress());
+        return pstm.executeUpdate()>0;
+
+    }
+
+}
