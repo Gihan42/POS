6c758decbfb04a960191435fa463555e9ddd6136
Gihan42 <gihanm729@gmail.com>
2022-04-29 11:31:11 +0530

    refactor update method

--- Layered_Architecture/src/dao/CustomerDAOImpl.java
+++ Layered_Architecture/src/dao/CustomerDAOImpl.java
@@ -29,5 +29,12 @@ public class CustomerDAOImpl {
         return pstm.executeUpdate()>0;
 
     }
-
+  public boolean updateCustomer(CustomerDTO dto) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+      PreparedStatement pstm = connection.prepareStatement("UPDATE Customer SET name=?, address=? WHERE id=?");
+      pstm.setString(1,dto.getId() );
+      pstm.setString(2, dto.getName());
+      pstm.setString(3, dto.getAddress());
+      return pstm.executeUpdate()>0;
+    }
 }
